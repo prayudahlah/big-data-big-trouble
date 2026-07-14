ad9e3a189c3516c956b7c602f15f3c141b616ff0
Trisha Garnis <trishagarniss@gmail.com>
2026-07-14 19:26:10 +0700

    .

--- modules/training/evaluate.py
+++ modules/training/evaluate.py
@@ -2,14 +2,14 @@ from sklearn.metrics import f1_score, precision_score, recall_score
 
 
 def compute_metrics(y_true, y_pred, num_classes=3):
-    f1_macro = f1_score(y_true, y_pred, average="macro")
+    f1_macro = f1_score(y_true, y_pred, average="macro", zero_division=0)
     f1_per_class = f1_score(
-        y_true, y_pred, average=None, labels=list(range(num_classes))
+        y_true, y_pred, average=None, labels=list(range(num_classes)), zero_division=0
     ).tolist()
     precision_per_class = precision_score(
-        y_true, y_pred, average=None, labels=list(range(num_classes))
+        y_true, y_pred, average=None, labels=list(range(num_classes)), zero_division=0
     ).tolist()
     recall_per_class = recall_score(
-        y_true, y_pred, average=None, labels=list(range(num_classes))
+        y_true, y_pred, average=None, labels=list(range(num_classes)), zero_division=0
     ).tolist()
     return f1_macro, f1_per_class, precision_per_class, recall_per_class
